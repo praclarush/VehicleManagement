2d7641c50328676dcc730aa6443e79472e79c271
Nathan Bremmer <praclarushfire@gmail.com>
2021-07-10 20:41:09 -0500

    - Added More Information for Vehicle

--- VehicleManagement.Database/dbo/Tables/Vehicle.sql
+++ VehicleManagement.Database/dbo/Tables/Vehicle.sql
@@ -6,5 +6,7 @@
     [Make] NCHAR(50) NOT NULL, 
     [Model] NCHAR(50) NOT NULL, 
     [Odometer] BIGINT NOT NULL, 
+    [VIN] VARCHAR(17) NULL, 
+    [LicensePlate] VARCHAR(20) NULL, 
     CONSTRAINT [FK_Vehicle_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]) 
 )
