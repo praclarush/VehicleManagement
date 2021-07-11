b10673de4294d3b390d6d7af988239a244e7e2d7
Nathan Bremmer <praclarushfire@gmail.com>
2021-07-10 20:38:21 -0500

    Creating Database Project - Loaded in Identity Tables - Started Vehicle Tables.

---
+++ VehicleManagement.Database/dbo/Tables/Vehicle.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Vehicle]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] NVARCHAR(450) NOT NULL, 
+    [FriendlyName] VARCHAR(50) NOT NULL, 
+    [Make] NCHAR(50) NOT NULL, 
+    [Model] NCHAR(50) NOT NULL, 
+    [Odometer] BIGINT NOT NULL, 
+    CONSTRAINT [FK_Vehicle_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]) 
+)
---
+++ VehicleManagement.Database/dbo/Tables/VehicleImage.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[VehicleImage]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [VehicleId] INT NULL, 
+    [ImageData] VARBINARY(MAX) NOT NULL, 
+    CONSTRAINT [FK_VehicleImage_Vehicle] FOREIGN KEY (VehicleID) REFERENCES [Vehicle]([Id])
+)
