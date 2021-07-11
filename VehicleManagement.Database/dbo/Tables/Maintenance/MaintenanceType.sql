90ca4da9a1fb5a2a32581d0206b049ba0fd73358
Nathan Bremmer <praclarushfire@gmail.com>
2021-07-11 11:10:06 -0500

    Added MIssing Refernece to Vehicle on MaintenanceType

--- VehicleManagement.Database/dbo/Tables/Maintenance/MaintenanceType.sql
+++ VehicleManagement.Database/dbo/Tables/Maintenance/MaintenanceType.sql
@@ -1,8 +1,14 @@
 ﻿CREATE TABLE [dbo].[MaintenanceType]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [VehicleId] INT NOT NULL,
     [TypeName] VARCHAR(50) NOT NULL, 
     [FriendlyName] VARCHAR(50) NOT NULL,
     [IsDeletable ] BIT NOT NULL, 
-    [IsDeleted] BIT NOT NULL
+    [IsDeleted] BIT NOT NULL, 
+    CONSTRAINT [FK_MaintenanceType_Vehicle] FOREIGN KEY ([VehicleId]) REFERENCES [Vehicle]([Id])
 )
+
+GO
+
+CREATE INDEX [IX_MaintenanceType_VehicleId] ON [dbo].[MaintenanceType] ([VehicleId])
